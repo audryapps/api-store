7ca2df59e9549932d4d7b5979700325c8cf22236
audryapps <manuelavargasherrera@gmail.com>
2020-01-20 11:14:20 -0500

    base store 2

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
+const PORT = process.env.PORT || 3000;
 const app = express()
-const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/products', (req, res) => res.send('Hello products!'))
+
+app.listen(PORT, () => console.log(`Example app runnig`))
