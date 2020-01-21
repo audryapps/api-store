96990b85b39a406367dfc20e95a377dff28f7faa
audryapps <manuelavargasherrera@gmail.com>
2020-01-20 20:42:24 -0500

    local

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,14 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://localhost:/api_store'
+    connection: 'postgres://localhost/api_store'
+  },
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/api_store'
   },
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL
   }
 };
-
-  
